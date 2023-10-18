41e47223c0b99e3af24014b427ebe5ac698ca653
gulw <gu.lw@foxmail.com>
2023-10-18 22:07:55 +0800

    feat: buffer

--- src/examples/multi-matrices.ts
+++ src/examples/multi-matrices.ts
@@ -1,6 +1,5 @@
 import { init } from "../init";
 import { initShader } from "../utils";
-import { mat4, glMatrix } from "gl-matrix";
 
 export function multiMatricesDemo() {
   const canvas = init();
@@ -9,62 +8,80 @@ export function multiMatricesDemo() {
 
   let vertexShader = `
   attribute vec3 a_position;
-  uniform mat4 u_t_matrix;
-  uniform mat4 u_s_matrix;
-  uniform mat4 u_r_matrix;
+  attribute vec3 a_color;
+  varying vec3 v_color;
 
   void main() {
-    // mat4 modelMatrix = u_t_matrix;
-    mat4 modelMatrix = u_t_matrix * u_s_matrix * u_r_matrix;
-    gl_Position = modelMatrix * vec4(a_position, 1.0);
+    v_color = a_color;
+    gl_Position = vec4(a_position, 1.0);
+    gl_PointSize = 10.0;
   }
   `;
 
   let fragmentShader = `
   precision mediump float;
+  varying vec3 v_color;
+  // gl_FragCoord: canvas 画布坐标（100， 100）
 
   void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    // gl_FragColor = vec4(v_color, 1.0);
+    gl_FragColor = vec4(gl_FragCoord.y / 400.0, 0.0, 0.0, 1.0);
   }
   `;
 
   const { program } = initShader(ctx, vertexShader, fragmentShader);
 
-  let vertices = new Float32Array([
-    -0.2, -0.2, 0.0, 0.2, -0.2, 0.0, 0.0, 0.2, 0.0,
-  ]);
 
-  let F_SIZE = vertices.BYTES_PER_ELEMENT;
+  function initVertexBuffer(gl: WebGLRenderingContext) {
+    let vertices = new Float32Array([
+      -0.5, 0.5, 0.0, 1.0, 0.0, 0.0,
+      -0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+      0.5, -0.5, 0.0, 0.0, 0.0, 1.0,
+      0.5, 0.5, 0.0, 1.0, 1.0, 1.0,
+    ]);
+    // let colors = new Float32Array([
+    //   1.0, 0.0, 0.0,
+    //   0.0, 1.0, 0.0,
+    //   0.0, 0.0, 1.0,
+    //   1.0, 1.0, 1.0,
+    // ])
+    let F_SIZE = vertices.BYTES_PER_ELEMENT;
 
-  let buffer = ctx.createBuffer()!;
-  ctx.bindBuffer(ctx.ARRAY_BUFFER, buffer);
-  ctx.bufferData(ctx.ARRAY_BUFFER, vertices, ctx.STATIC_DRAW);
+    let buffer = gl.createBuffer()!
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
-  let a_position = ctx.getAttribLocation(program, "a_position");
-  ctx.vertexAttribPointer(a_position, 3, ctx.FLOAT, false, 3 * F_SIZE, 0);
-  ctx.enableVertexAttribArray(a_position);
+    let a_position = gl.getAttribLocation(program, "a_position");
+    gl.vertexAttribPointer(a_position, 3, gl.FLOAT, false, 6 * F_SIZE, 0);
+    gl.enableVertexAttribArray(a_position);
 
-  let tMatrix = mat4.create();
-  let sMatrix = mat4.create();
-  let rMatrix = mat4.create();
+    let a_color = gl.getAttribLocation(program, "a_color");
+    gl.vertexAttribPointer(a_color, 3, gl.FLOAT, false, 6 * F_SIZE, F_SIZE * 3);
+    gl.enableVertexAttribArray(a_color);
 
-  mat4.fromTranslation(tMatrix, [0.5, 0.0, 0.0]);
-  mat4.fromScaling(sMatrix, [2, 1, 1]);
-  mat4.fromRotation(rMatrix, glMatrix.toRadian(10), [0, 0, 1]);
+    // let positionsBuffer = gl.createBuffer()!
+    // gl.bindBuffer(gl.ARRAY_BUFFER, positionsBuffer);
+    // gl.bufferData(gl.ARRAY_BUFFER, position, gl.STATIC_DRAW);
 
-  let u_tMatrix = ctx.getUniformLocation(program, "u_t_matrix");
-  ctx.uniformMatrix4fv(u_tMatrix, false, tMatrix);
+    // let a_position = gl.getAttribLocation(program, "a_position");
+    // gl.vertexAttribPointer(a_position, 3, gl.FLOAT, false, 3 * F_SIZE, 0);
+    // gl.enableVertexAttribArray(a_position);
 
-  let u_sMatrix = ctx.getUniformLocation(program, "u_s_matrix");
-  ctx.uniformMatrix4fv(u_sMatrix, false, sMatrix);
+    // let colorsBuffer = gl.createBuffer()!
+    // gl.bindBuffer(gl.ARRAY_BUFFER, colorsBuffer);
+    // gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
 
-  let u_rMatrix = ctx.getUniformLocation(program, "u_r_matrix");
-  ctx.uniformMatrix4fv(u_rMatrix, false, rMatrix);
+    // let a_color = gl.getAttribLocation(program, "a_color");
+    // gl.vertexAttribPointer(a_color, 3, gl.FLOAT, false, 3 * F_SIZE, 0);
+    // gl.enableVertexAttribArray(a_color);
+  }
+
+  initVertexBuffer(ctx);
 
   function draw(ctx: WebGLRenderingContext) {
     ctx.clearColor(0.0, 0.0, 0.0, 1.0);
     ctx.clear(ctx.COLOR_BUFFER_BIT);
-    ctx.drawArrays(ctx.TRIANGLES, 0, 3);
+    ctx.drawArrays(ctx.TRIANGLE_FAN, 0, 4);
   }
 
   draw(ctx);
